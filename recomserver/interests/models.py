9cfb92bc75651acc49eea91bf51851945b9c2b98
Aybars Badur <aybars.badur@mediamonks.com>
2017-02-22 18:06:29 +0100

    hashing models

--- recomserver/interests/models.py
+++ recomserver/interests/models.py
@@ -1,5 +1,23 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
-# Create your models here.
+
+
+class Interest(models.Model):
+    name = models.CharField(_('Interest Name'), max_length=255)
+
+class InterestUser(models.Model):
+    interest = models.ForeignKey(Interest)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+class UserInterestHash(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    phash = models.BigIntegerField()
+    phash_hex = models.TextField(default=None, null=True)
+
+
